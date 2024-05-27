b926b7362483a1aea7807859474eacac894d284e
EZEEJAIN <jezeejain123455@gmail.com>
2024-05-27 15:05:49 +0530

    initial commit

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,11 +1,15 @@
 const express = require("express");
-const { getUsers, postUsers,postUsersLogin, } = require("../controller/userController");
+const {
+  getUsers,
+  postUsers,
+  postUsersLogin,
+} = require("../controller/userController");
 
 const router = express.Router();
 
-router.get("/users", getUsers)
-router.post("/signup", postUsers)
-router.post("/login", postUsersLogin)
+router.get("/users", getUsers);
+router.post("/signup", postUsers);
+router.post("/login", postUsersLogin);
 // router.patch("/users", updateUsers)
 // router.delete("/users", getUsers)
 
